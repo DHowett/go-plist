effb7865bf3959a691b55cd4a2239505fb106f67
Dustin L. Howett <dustin@howett.net>
2013-11-26 00:21:26 -0800

    Add an XML->plistValue decoder.

--- xml.go
+++ xml.go
@@ -3,7 +3,9 @@ package plist
 import (
 	"encoding/base64"
 	"encoding/xml"
+	"errors"
 	"io"
+	"strconv"
 )
 
 type xmlPlistValueEncoder struct {
@@ -84,4 +86,152 @@ func (p *xmlPlistValueEncoder) encodePlistValue(plistVal *plistValue) error {
 
 func newXMLPlistValueEncoder(w io.Writer) *xmlPlistValueEncoder {
 	return &xmlPlistValueEncoder{w, xml.NewEncoder(w)}
+}
+
+type xmlPlistValueDecoder struct {
+	reader     io.Reader
+	xmlDecoder *xml.Decoder
+}
+
+func (p *xmlPlistValueDecoder) decodeDocument() (*plistValue, error) {
+	for {
+		if token, err := p.xmlDecoder.Token(); err == nil {
+			if element, ok := token.(xml.StartElement); ok {
+				return p.decodeXMLElement(element)
+			}
+		} else {
+			return nil, err
+		}
+	}
+	return nil, nil
+}
+
+func (p *xmlPlistValueDecoder) decodeXMLElement(element xml.StartElement) (*plistValue, error) {
+	var charData xml.CharData
+	switch element.Name.Local {
+	case "plist":
+		for {
+			token, err := p.xmlDecoder.Token()
+			if err != nil {
+				return nil, err
+			}
+
+			if el, ok := token.(xml.EndElement); ok && el.Name.Local == "plist" {
+				break
+			}
+
+			if el, ok := token.(xml.StartElement); ok {
+				return p.decodeXMLElement(el)
+			}
+		}
+		return nil, nil
+	case "string":
+		err := p.xmlDecoder.DecodeElement(&charData, &element)
+		if err != nil {
+			return nil, err
+		}
+
+		return &plistValue{String, string(charData)}, nil
+	case "integer":
+		err := p.xmlDecoder.DecodeElement(&charData, &element)
+		if err != nil {
+			return nil, err
+		}
+
+		n, err := strconv.ParseUint(string(charData), 10, 64)
+		if err != nil {
+			return nil, err
+		}
+
+		return &plistValue{Integer, n}, nil
+	case "real":
+		err := p.xmlDecoder.DecodeElement(&charData, &element)
+		if err != nil {
+			return nil, err
+		}
+
+		n, err := strconv.ParseFloat(string(charData), 64)
+		if err != nil {
+			return nil, err
+		}
+
+		return &plistValue{Real, n}, nil
+	case "true", "false":
+		p.xmlDecoder.Skip()
+
+		b := false
+		if element.Name.Local == "true" {
+			b = true
+		}
+		return &plistValue{Boolean, b}, nil
+	case "data":
+		err := p.xmlDecoder.DecodeElement(&charData, &element)
+		if err != nil {
+			return nil, err
+		}
+
+		l := base64.StdEncoding.DecodedLen(len(charData))
+		bytes := make([]uint8, l)
+		l, err = base64.StdEncoding.Decode(bytes, charData)
+		if err != nil {
+			return nil, err
+		}
+
+		return &plistValue{Data, bytes[:l]}, nil
+	case "dict":
+		var key string
+		var subvalues map[string]*plistValue = make(map[string]*plistValue)
+		for {
+			token, err := p.xmlDecoder.Token()
+			if err != nil {
+				return nil, err
+			}
+
+			if el, ok := token.(xml.EndElement); ok && el.Name.Local == "dict" {
+				break
+			}
+
+			if el, ok := token.(xml.StartElement); ok {
+				if el.Name.Local == "key" {
+					p.xmlDecoder.DecodeElement(&key, &el)
+				} else {
+					if key == "" {
+						return nil, errors.New("Missing key for value")
+					}
+					sval, err := p.decodeXMLElement(el)
+					if err != nil {
+						return nil, err
+					}
+					subvalues[key] = sval
+				}
+			}
+		}
+		return &plistValue{Dictionary, subvalues}, nil
+	case "array":
+		var subvalues []*plistValue = make([]*plistValue, 0, 10)
+		for {
+			token, err := p.xmlDecoder.Token()
+			if err != nil {
+				return nil, err
+			}
+
+			if el, ok := token.(xml.EndElement); ok && el.Name.Local == "array" {
+				break
+			}
+
+			if el, ok := token.(xml.StartElement); ok {
+				sval, err := p.decodeXMLElement(el)
+				if err != nil {
+					return nil, err
+				}
+				subvalues = append(subvalues, sval)
+			}
+		}
+		return &plistValue{Array, subvalues}, nil
+	}
+	return nil, nil
+}
+
+func newXMLPlistValueDecoder(r io.Reader) *xmlPlistValueDecoder {
+	return &xmlPlistValueDecoder{r, xml.NewDecoder(r)}
 }
