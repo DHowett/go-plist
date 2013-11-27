e7c12150696307649555ab9e1304985b633ace8f
Dustin L. Howett <dustin@howett.net>
2013-11-27 01:57:37 -0800

    [encode_test] Lift out common XML preamble junk.

--- encode_test.go
+++ encode_test.go
@@ -22,12 +22,14 @@ type SparseBundleHeader struct {
 	Size                  uint64 `plist:"size"`
 }
 
+var xmlPreamble string = `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">`
+
 var tests = []EncodingTest{
 	{
-		Name: "String",
-		Data: "Hello",
-		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><string>Hello</string></plist>`,
+		Name:           "String",
+		Data:           "Hello",
+		ExpectedResult: xmlPreamble + `<plist version="1.0"><string>Hello</string></plist>`,
 	},
 	{
 		Name: "Basic Structure",
@@ -36,32 +38,27 @@ var tests = []EncodingTest{
 		}{
 			Name: "Dustin",
 		},
-		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><dict><key>Name</key><string>Dustin</string></dict></plist>`,
+		ExpectedResult: xmlPreamble + `<plist version="1.0"><dict><key>Name</key><string>Dustin</string></dict></plist>`,
 	},
 	{
-		Name: "Arbitrary Byte Data",
-		Data: []byte{'h', 'e', 'l', 'l', 'o'},
-		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><data>aGVsbG8=</data></plist>`,
+		Name:           "Arbitrary Byte Data",
+		Data:           []byte{'h', 'e', 'l', 'l', 'o'},
+		ExpectedResult: xmlPreamble + `<plist version="1.0"><data>aGVsbG8=</data></plist>`,
 	},
 	{
-		Name: "Arbitrary Integer Array",
-		Data: []int{'h', 'e', 'l', 'l', 'o'},
-		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><array><integer>104</integer><integer>101</integer><integer>108</integer><integer>108</integer><integer>111</integer></array></plist>`,
+		Name:           "Arbitrary Integer Array",
+		Data:           []int{'h', 'e', 'l', 'l', 'o'},
+		ExpectedResult: xmlPreamble + `<plist version="1.0"><array><integer>104</integer><integer>101</integer><integer>108</integer><integer>108</integer><integer>111</integer></array></plist>`,
 	},
 	{
-		Name: "Boolean True",
-		Data: true,
-		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><true></true></plist>`,
+		Name:           "Boolean True",
+		Data:           true,
+		ExpectedResult: xmlPreamble + `<plist version="1.0"><true></true></plist>`,
 	},
 	{
-		Name: "Floating-Point Value",
-		Data: math.Pi,
-		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><real>3.141592653589793</real></plist>`,
+		Name:           "Floating-Point Value",
+		Data:           math.Pi,
+		ExpectedResult: xmlPreamble + `<plist version="1.0"><real>3.141592653589793</real></plist>`,
 	},
 	{
 		Name: "Map (containing arbitrary types)",
@@ -69,8 +66,7 @@ var tests = []EncodingTest{
 			"float":  1.0,
 			"uint64": uint64(1),
 		},
-		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><dict><key>float</key><real>1</real><key>uint64</key><integer>1</integer></dict></plist>`,
+		ExpectedResult: xmlPreamble + `<plist version="1.0"><dict><key>float</key><real>1</real><key>uint64</key><integer>1</integer></dict></plist>`,
 	},
 	{
 		Name:       "Map (integer keys) (expected to fail)",
@@ -86,8 +82,7 @@ var tests = []EncodingTest{
 			DiskImageBundleType:   "com.apple.diskimage.sparsebundle",
 			BackingStoreVersion:   1,
 		},
-		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><dict><key>CFBundleInfoDictionaryVersion</key><string>6.0</string><key>band-size</key><integer>8388608</integer><key>bundle-backingstore-version</key><integer>1</integer><key>diskimage-bundle-type</key><string>com.apple.diskimage.sparsebundle</string><key>size</key><integer>4398046511104</integer></dict></plist>`,
+		ExpectedResult: xmlPreamble + `<plist version="1.0"><dict><key>CFBundleInfoDictionaryVersion</key><string>6.0</string><key>band-size</key><integer>8388608</integer><key>bundle-backingstore-version</key><integer>1</integer><key>diskimage-bundle-type</key><string>com.apple.diskimage.sparsebundle</string><key>size</key><integer>4398046511104</integer></dict></plist>`,
 	},
 	{
 		Name: "Array of byte arrays",
@@ -95,26 +90,22 @@ var tests = []EncodingTest{
 			[]byte("Hello"),
 			[]byte("World"),
 		},
-		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><array><data>SGVsbG8=</data><data>V29ybGQ=</data></array></plist>`,
+		ExpectedResult: xmlPreamble + `<plist version="1.0"><array><data>SGVsbG8=</data><data>V29ybGQ=</data></array></plist>`,
 	},
 	{
-		Name: "Date",
-		Data: time.Date(2013, 11, 27, 0, 34, 0, 0, time.UTC),
-		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><date>2013-11-27T00:34:00Z</date></plist>`,
+		Name:           "Date",
+		Data:           time.Date(2013, 11, 27, 0, 34, 0, 0, time.UTC),
+		ExpectedResult: xmlPreamble + `<plist version="1.0"><date>2013-11-27T00:34:00Z</date></plist>`,
 	},
 	{
-		Name: "Floating-Point NaN",
-		Data: math.NaN(),
-		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><real>nan</real></plist>`,
+		Name:           "Floating-Point NaN",
+		Data:           math.NaN(),
+		ExpectedResult: xmlPreamble + `<plist version="1.0"><real>nan</real></plist>`,
 	},
 	{
-		Name: "Floating-Point Infinity",
-		Data: math.Inf(1),
-		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><real>inf</real></plist>`,
+		Name:           "Floating-Point Infinity",
+		Data:           math.Inf(1),
+		ExpectedResult: xmlPreamble + `<plist version="1.0"><real>inf</real></plist>`,
 	},
 }
 
