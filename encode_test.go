8cdcedd2d2047aa25afe8a14602db7334a3ad553
Dustin L. Howett <dustin@howett.net>
2013-11-27 00:39:55 -0800

    Actually make the tests work (they were written before we were emitting doctypes).

--- encode_test.go
+++ encode_test.go
@@ -2,7 +2,9 @@ package plist
 
 import (
 	"bytes"
+	"math"
 	"testing"
+	"time"
 )
 
 type EncodingTest struct {
@@ -21,34 +23,46 @@ type SparseBundleHeader struct {
 
 var tests = []EncodingTest{
 	{
+		Data: "Hello",
+		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><string>Hello</string></plist>`,
+	},
+	{
 		Data: struct {
 			Name string
 		}{
 			Name: "Dustin",
 		},
-		ExpectedResult: "<dict><key>Name</key><string>Dustin</string></dict>",
+		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><dict><key>Name</key><string>Dustin</string></dict></plist>`,
 	},
 	{
-		Data:           "Hello",
-		ExpectedResult: "<string>Hello</string>",
+		Data: []byte{'h', 'e', 'l', 'l', 'o'},
+		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><data>aGVsbG8=</data></plist>`,
 	},
 	{
 		Data: []int{'h', 'e', 'l', 'l', 'o'},
+		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><array><integer>104</integer><integer>101</integer><integer>108</integer><integer>108</integer><integer>111</integer></array></plist>`,
 	},
 	{
 		Data: true,
+		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><true></true></plist>`,
 	},
 	{
 		Data: 1.0,
-	},
-	{
-		Data: []byte("Hello base64 data!"),
+		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><real>1</real></plist>`,
 	},
 	{
 		Data: map[string]interface{}{
 			"float":  1.0,
 			"uint64": uint64(1),
 		},
+		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><dict><key>float</key><real>1</real><key>uint64</key><integer>1</integer></dict></plist>`,
 	},
 	{
 		Data:       map[int]string{1: "hi"},
@@ -62,14 +76,26 @@ var tests = []EncodingTest{
 			DiskImageBundleType:   "com.apple.diskimage.sparsebundle",
 			BackingStoreVersion:   1,
 		},
-		ExpectedResult: "<dict><key>CFBundleInfoDictionaryVersion</key><string>6.0</string><key>band-size</key><integer>8388608</integer><key>bundle-backingstore-version</key><integer>1</integer><key>diskimage-bundle-type</key><string>com.apple.diskimage.sparsebundle</string><key>size</key><integer>4398046511104</integer></dict>",
+		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><dict><key>CFBundleInfoDictionaryVersion</key><string>6.0</string><key>band-size</key><integer>8388608</integer><key>bundle-backingstore-version</key><integer>1</integer><key>diskimage-bundle-type</key><string>com.apple.diskimage.sparsebundle</string><key>size</key><integer>4398046511104</integer></dict></plist>`,
 	},
 	{
 		Data: [][]byte{
 			[]byte("Hello"),
 			[]byte("World"),
 		},
-		ExpectedResult: "<array><data>SGVsbG8=</data><data>V29ybGQ=</data></array>",
+		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><array><data>SGVsbG8=</data><data>V29ybGQ=</data></array></plist>`,
+	},
+	{
+		Data: time.Date(2013, 11, 27, 0, 34, 0, 0, time.UTC),
+		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><date>2013-11-27T00:34:00Z</date></plist>`,
+	},
+	{
+		Data: math.NaN(),
+		ExpectedResult: `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><real>nan</real></plist>`,
 	},
 }
 
@@ -78,24 +104,23 @@ func TestEncode(t *testing.T) {
 		buf := &bytes.Buffer{}
 		encoder := NewEncoder(buf)
 		err := encoder.Encode(test.Data)
-		t.Logf("Encoding %v", test.Data)
-		if test.ShouldFail {
-			t.Logf("Expected: Error")
-		} else {
-			if test.ExpectedResult != "" {
-				t.Log("Expected:", test.ExpectedResult)
-			}
-		}
-
-		if err == nil {
-			t.Log("Received:", buf.String())
-		} else {
-			t.Log("   Error:", err)
-		}
+		t.Logf("Encoding %#v", test.Data)
 
 		if (test.ExpectedResult != "" && test.ExpectedResult != buf.String()) || (test.ShouldFail && err == nil) {
+			if test.ShouldFail {
+				t.Logf("Expected: Error")
+			} else {
+				if test.ExpectedResult != "" {
+					t.Log("Expected:", test.ExpectedResult)
+				}
+			}
 			t.Log("FAILED")
 			t.Fail()
+			if err == nil {
+				t.Log("Received:", buf.String())
+			} else {
+				t.Log("   Error:", err)
+			}
 		} else {
 			t.Log("SUCCEEDED")
 		}
