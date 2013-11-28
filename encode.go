49ffa10223f1323ce0ba26eb21f0e5501bd20f77
Dustin L. Howett <dustin@howett.net>
2013-11-27 17:50:43 -0800

    [encode] Documentation!

--- encode.go
+++ encode.go
@@ -1,3 +1,6 @@
+// Package plist implements encoding and decoding of Apple's "property list" format.
+// Property lists come in two sorts: XML and Binary. plist decodes both, but can only write XML.
+// The mapping between property list and Go objects is described in the documentation for the Encode and Decode functions.
 package plist
 
 import (
@@ -12,11 +15,41 @@ type plistValueEncoder interface {
 	encodePlistValue(*plistValue)
 }
 
+// An Encoder writes a property list to an output stream.
 type Encoder struct {
 	writer       io.Writer
 	valueEncoder plistValueEncoder
 }
 
+// Encode writes the property list encoding of v to the connection.
+//
+// Encode traverses the value v recursively.
+// Any nil values encountered, other than the root, will be silently discarded as
+// the property list format bears no representation for nil values.
+//
+// Strings, integers of varying size, floats and booleans are encoded unchanged.
+//
+// Slice and Array values are encoded as property list arrays, except for
+// []byte values, which are encoded as data.
+//
+// Map values encode as dictionaries. The map's key type must be string; there is no provision for encoding non-string dictionary keys.
+//
+// Struct values are encoded as dictionaries, with only exported fields being serialized. Struct field encoding may be influenced with the use of tags.
+// The tag format is:
+//
+//     `plist:"<key>[,flags...]"`
+//
+// The following flags are supported:
+//
+//     omitempty    Only include the field if it is not set to the zero value for its type.
+//
+// If the key is "-", the field is ignored.
+//
+// Anonymous struct fields are encoded as if their exported fields were exposed via the outer struct.
+//
+// Pointer values encode as the value pointed to.
+//
+// Channel, complex and function values cannot be encoded. Any attempt to do so causes Encode to return an error.
 func (p *Encoder) Encode(v interface{}) (err error) {
 	defer func() {
 		if r := recover(); r != nil {
@@ -36,6 +69,7 @@ func (p *Encoder) Encode(v interface{}) (err error) {
 	return
 }
 
+// NewEncoder returns an Encoder that writes an XML property list to w
 func NewEncoder(w io.Writer) *Encoder {
 	p := &Encoder{
 		valueEncoder: newXMLPlistValueEncoder(w),
