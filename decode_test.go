bd052ba2549ce6ca31a480e4be4cc597b5ea468f
Dustin L. Howett <dustin@howett.net>
2013-11-29 22:17:11 -0800

    Add example functions.

--- decode_test.go
+++ decode_test.go
@@ -2,6 +2,7 @@ package plist
 
 import (
 	"bytes"
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -161,4 +162,41 @@ func TestDecode(t *testing.T) {
 			t.Fail()
 		}
 	}
+}
+
+func ExampleDecoder_Decode() {
+	type sparseBundleHeader struct {
+		InfoDictionaryVersion string `plist:"CFBundleInfoDictionaryVersion"`
+		BandSize              uint64 `plist:"band-size"`
+		BackingStoreVersion   int    `plist:"bundle-backingstore-version"`
+		DiskImageBundleType   string `plist:"diskimage-bundle-type"`
+		Size                  uint64 `plist:"size"`
+	}
+
+	buf := bytes.NewReader([]byte(`<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+	<dict>
+		<key>CFBundleInfoDictionaryVersion</key>
+		<string>6.0</string>
+		<key>band-size</key>
+		<integer>8388608</integer>
+		<key>bundle-backingstore-version</key>
+		<integer>1</integer>
+		<key>diskimage-bundle-type</key>
+		<string>com.apple.diskimage.sparsebundle</string>
+		<key>size</key>
+		<integer>4398046511104</integer>
+	</dict>
+</plist>`))
+
+	var data sparseBundleHeader
+	decoder := NewDecoder(buf)
+	err := decoder.Decode(&data)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(data)
+
+	// Output: {6.0 8388608 1 com.apple.diskimage.sparsebundle 4398046511104}
 }
