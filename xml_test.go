817771811f0853e9c1c6806bdbc4c232904f436f
Dustin L. Howett <dustin@howett.net>
2013-12-08 03:45:22 -0800

    [tests] Add tests for various invalid XML plists.

--- xml_test.go
+++ xml_test.go
@@ -23,3 +23,46 @@ func BenchmarkXMLParse(b *testing.B) {
 		buf.Seek(0, 0)
 	}
 }
+
+func TestVariousIllegalXMLPlists(t *testing.T) {
+	plists := []string{
+		"<plist><doct><key>helo</key><string></string></doct></plist>",
+		"<plist><dict><string>helo</string></dict></plist>",
+		"<plist><dict><key>helo</key></dict></plist>",
+		"<plist><integer>helo</integer></plist>",
+		"<plist><real>helo</real></plist>",
+		"<plist><data>*@&amp;%#helo</data></plist>",
+		"<plist><date>*@&amp;%#helo</date></plist>",
+		"<plist><date>*@&amp;%#helo</date></plist>",
+		"<plist><integer>10</plist>",
+		"<plist><real>10</plist>",
+		"<plist><string>10</plist>",
+		"<plist><dict>10</plist>",
+		"<plist><dict><key>10</plist>",
+		"<plist>",
+		"<plist><data>",
+		"<plist><date>",
+		"<plist><array>",
+		"<pl",
+	}
+
+	testDecode := func(plist string) (e error) {
+		defer func() {
+			if err := recover(); err != nil {
+				e = err.(error)
+			}
+		}()
+		buf := bytes.NewReader([]byte(plist))
+		d := newXMLPlistParser(buf)
+		d.parseDocument()
+		return nil
+	}
+
+	for _, plist := range plists {
+		err := testDecode(plist)
+		t.Logf("Error: %v", err)
+		if err == nil {
+			t.Error("Expected error, received nothing.")
+		}
+	}
+}
