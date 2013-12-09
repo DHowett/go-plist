164f2dca1cbfd4da0dfefacca6b9cc6953dea48a
Dustin L. Howett <dustin@howett.net>
2013-12-08 18:04:37 -0800

    Move package-level documentation to doc.go.

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Package plist implements encoding and decoding of Apple's "property list" format.
+// Property lists come in three sorts: plain text, XML and binary.
+// Plain text (OpenStep) property lists are not currently supported by plist.
+// The mapping between property list and Go objects is described in the documentation for the Encode and Decode functions.
+package plist
--- encode.go
+++ encode.go
@@ -1,7 +1,3 @@
-// Package plist implements encoding and decoding of Apple's "property list" format.
-// Property lists come in three sorts: plain text, XML and binary. plist can read and write both.
-// Plain text (OpenStep) property lists are not currently supported.
-// The mapping between property list and Go objects is described in the documentation for the Encode and Decode functions.
 package plist
 
 import (
