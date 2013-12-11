77ac7cce57dd8bf6206cd7b57bf20ae855c6207e
Dustin L. Howett <dustin@howett.net>
2013-12-11 01:44:33 -0800

    [tests] Add an OpenStep Parse benchmark (using the same pathological data.)

--- text_test.go
+++ text_test.go
@@ -1,6 +1,7 @@
 package plist
 
 import (
+	"bytes"
 	"testing"
 )
 
@@ -8,5 +9,17 @@ func BenchmarkOpenStepGenerate(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		d := &textPlistGenerator{nilWriter(0)}
 		d.generateDocument(plistValueTree)
+	}
+}
+
+func BenchmarkOpenStepParse(b *testing.B) {
+	buf := bytes.NewReader([]byte(plistValueTreeAsOpenStep))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b.StartTimer()
+		d := newTextPlistParser(buf)
+		d.parseDocument()
+		b.StopTimer()
+		buf.Seek(0, 0)
 	}
 }
