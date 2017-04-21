294a0ce6ddd1c2b356d5fdd71382c4bdc2bbd3b0
Daniil Zinenko <danil@nulana.com>
2017-04-22 04:45:08 +0500

    bplist latin-1 to utf16 test

--- bplist_test.go
+++ bplist_test.go
@@ -2,6 +2,7 @@ package plist
 
 import (
 	"bytes"
+	"encoding/binary"
 	"io/ioutil"
 	"testing"
 )
@@ -33,5 +34,49 @@ func TestBplistInt128(t *testing.T) {
 	pval, _ := d.parseDocument()
 	if pinteger, ok := pval.(*cfNumber); !ok || pinteger.value != expected {
 		t.Error("Expected", expected, "received", pval)
+	}
+}
+
+func TestBplistLatin1ToUTF16(t *testing.T) {
+	sBuf := make([]byte, 2)
+	expected := []byte{0x62, 0x70, 0x6c, 0x69, 0x73, 0x74, 0x30, 0x30, 0xd1, 0x01, 0x02, 0x51, 0x5f, 0x61, 0x00, 0x80, 0x08, 0x0b, 0x0d, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10}
+
+	var buf bytes.Buffer
+	encoder := NewBinaryEncoder(&buf)
+
+	for i := uint16(0xc280); i <= 0xc2bf; i++ {
+		binary.BigEndian.PutUint16(sBuf, i)
+		data := map[string]string{
+			"_": string(sBuf),
+		}
+		expected[15] = sBuf[1]
+
+		buf.Reset()
+		if err := encoder.Encode(data); err != nil {
+			t.Error(err.Error())
+		}
+
+		if !bytes.Equal(buf.Bytes(), expected) {
+			t.Errorf("Expected", expected, "received", buf.Bytes())
+			return
+		}
+	}
+
+	for i := uint16(0xc380); i <= 0xc3bf; i++ {
+		binary.BigEndian.PutUint16(sBuf, i)
+		data := map[string]string{
+			"_": string(sBuf),
+		}
+		expected[15] = sBuf[1] + 0x40
+
+		buf.Reset()
+		if err := encoder.Encode(data); err != nil {
+			t.Error(err.Error())
+		}
+
+		if !bytes.Equal(buf.Bytes(), expected) {
+			t.Errorf("Expected", expected, "received", buf.Bytes())
+			return
+		}
 	}
 }
