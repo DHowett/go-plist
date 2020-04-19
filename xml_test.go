c2293a37d45736de8fb259e3efc519d332c5d636
Dustin L. Howett <dustin@howett.net>
2020-04-19 14:37:33 -0700

    test: whoops, forgot to commit part of the change for dump

--- xml_test.go
+++ xml_test.go
@@ -25,33 +25,33 @@ func BenchmarkXMLParse(b *testing.B) {
 	}
 }
 
-func TestVariousIllegalXMLPlists(t *testing.T) {
-	plists := []string{
-		`<plist version="1.0"><integer>0x</integer></plist>`,
-		"<plist><doct><key>helo</key><string></string></doct></plist>",
-		"<plist><dict><string>helo</string></dict></plist>",
-		"<plist><dict><key>helo</key></dict></plist>",
-		"<plist><integer>helo</integer></plist>",
-		"<plist><integer></integer></plist>",
-		"<plist><real>helo</real></plist>",
-		"<plist><data>*@&amp;%#helo</data></plist>",
-		"<plist><date>*@&amp;%#helo</date></plist>",
-		"<plist><date>*@&amp;%#helo</date></plist>",
-		"<plist><integer>10</plist>",
-		"<plist><real>10</plist>",
-		"<plist><string>10</plist>",
-		"<plist><dict>10</plist>",
-		"<plist><dict><key>10</plist>",
-		"<plist>",
-		"<plist><data>",
-		"<plist><date>",
-		"<plist><array>",
-		"<plist/>",
-		"<pl",
-		"bplist00",
-	}
+var InvalidXMLPlists = []string{
+	`<plist version="1.0"><integer>0x</integer></plist>`,
+	"<plist><doct><key>helo</key><string></string></doct></plist>",
+	"<plist><dict><string>helo</string></dict></plist>",
+	"<plist><dict><key>helo</key></dict></plist>",
+	"<plist><integer>helo</integer></plist>",
+	"<plist><integer></integer></plist>",
+	"<plist><real>helo</real></plist>",
+	"<plist><data>*@&amp;%#helo</data></plist>",
+	"<plist><date>*@&amp;%#helo</date></plist>",
+	"<plist><date>*@&amp;%#helo</date></plist>",
+	"<plist><integer>10</plist>",
+	"<plist><real>10</plist>",
+	"<plist><string>10</plist>",
+	"<plist><dict>10</plist>",
+	"<plist><dict><key>10</plist>",
+	"<plist>",
+	"<plist><data>",
+	"<plist><date>",
+	"<plist><array>",
+	"<plist/>",
+	"<pl",
+	"bplist00",
+}
 
-	for _, plist := range plists {
+func TestVariousIllegalXMLPlists(t *testing.T) {
+	for _, plist := range InvalidXMLPlists {
 		buf := bytes.NewReader([]byte(plist))
 		d := newXMLPlistParser(buf)
 		obj, err := d.parseDocument()
